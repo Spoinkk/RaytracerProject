f5bc5e70a14ff7cbe34bc8fd49378da1ab16872c
Spencer Robichaud <s_obich@willpower.encs.concordia.ca>
2024-02-07 16:15:57 -0500

    second commit

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -2,56 +2,20 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
 
-using namespace std;
-
-int save_ppm(std::string file_name, const std::vector<double>& buffer, int dimx, int dimy) {
-
-    ofstream ofs(file_name, ios_base::out | ios_base::binary);
-    ofs << "P6" << endl << dimx << ' ' << dimy << endl << "255" << endl;
-
-    for (unsigned int j = 0; j < dimy; ++j)
-        for (unsigned int i = 0; i < dimx; ++i)
-            ofs << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 0]) << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 1]) << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 2]);
-
-    ofs.close();
-
-    return 0;
-}
-
-std::vector<double> set_buffer(int dimX, int dimY, int w) {
-
-    std::vector<double> buffer(3 * dimX * dimY);
-    for (int j = 0; j < dimY; ++j) {
-        for (int i = 0; i < dimY; ++i) {
-            if (((i + j) / w) % 2 == 0) {
-                buffer[3 * j * dimX + 3 * i + 0] = 1;
-                buffer[3 * j * dimX + 3 * i + 1] = 1;
-                buffer[3 * j * dimX + 3 * i + 2] = 0;
-            }
-            else {
-                buffer[3 * j * dimX + 3 * i + 0] = 0;
-                buffer[3 * j * dimX + 3 * i + 1] = 1;
-                buffer[3 * j * dimX + 3 * i + 2] = 1;
-            }
-        }
-    }
-
-    return buffer;
-}
 
 
 RayTracer::RayTracer(nlohmann::json& j){
-  
+    
     int dimX = 800;
     int dimY = 600;
     int w = 100;
     
     std::vector<double> buffer = set_buffer(dimX, dimY, w);
+    std::string filename = parse_filename(j);
 
 
 
-
-    save_ppm(j, buffer, dimX, dimY);
+    sv_ppm(filename, buffer, dimX, dimY);
     
 }
 
--- RayTracer.h
+++ RayTracer.h
@@ -4,10 +4,11 @@
 #include "../external/json.hpp"
 #include <Eigen/Core>
 #include <Eigen/Dense>
-#include <IOStream>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 using namespace std;
+using namespace nlohmann;
 
 class RayTracer {
   public:
@@ -15,7 +16,63 @@ class RayTracer {
 
     RayTracer(nlohmann::json& j);
 
-    int save_ppm(std::string file_name, const std::vector<double>& buffer, int dimx, int dimy);
+        int sv_ppm(std::string file_name, const std::vector<double>& buffer, int dimx, int dimy) {
+
+        ofstream ofs(file_name, ios_base::out | ios_base::binary);
+        ofs << "P6" << endl << dimx << ' ' << dimy << endl << "255" << endl;
+
+        for (unsigned int j = 0; j < dimy; ++j)
+            for (unsigned int i = 0; i < dimx; ++i)
+                ofs << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 0]) << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 1]) << (char)(255.0 * buffer[3 * j * dimx + 3 * i + 2]);
+
+        ofs.close();
+
+        return 0;
+    }
+
+    std::vector<double> set_buffer(int dimX, int dimY, int w) {
+
+        std::vector<double> buffer(3 * dimX * dimY);
+        for (int j = 0; j < dimY; ++j) {
+            for (int i = 0; i < dimY; ++i) {
+                if (((i + j) / w) % 2 == 0) {
+                    buffer[3 * j * dimX + 3 * i + 0] = 1;
+                    buffer[3 * j * dimX + 3 * i + 1] = 1;
+                    buffer[3 * j * dimX + 3 * i + 2] = 0;
+                }
+                else {
+                    buffer[3 * j * dimX + 3 * i + 0] = 0;
+                    buffer[3 * j * dimX + 3 * i + 1] = 1;
+                    buffer[3 * j * dimX + 3 * i + 2] = 1;
+                }
+            }
+        }
+
+        return buffer;
+    }
+
+    std::string parse_filename(json& j) {
+        cout << "Outputs: " << endl;
+        int lc = 0;
+
+        // use iterators to read-in array types
+        for (auto itr = j["output"].begin(); itr != j["output"].end(); itr++) {
+
+            std::string filename;
+            if (itr->contains("filename")) {
+                //  filename = static_cast<std::string>((*itr)["filename"]);
+                filename = (*itr)["filename"].get<std::string>();
+            }
+            else {
+                cout << "Fatal error: output shoudl always contain a filename!!!" << endl;
+                return "";
+            }
+
+            cout << "Filename: " << filename << endl;
+            return filename;
+        }
+       
+    }
     
     void run();
     
